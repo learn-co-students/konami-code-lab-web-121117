f9bac0be3b9cde1f91947d1517c1c6fd1a2681c8
julientregoat <julienctregoat@gmail.com>
2018-02-01 10:00:04 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let idx = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.querySelector('body')
 
-}
+  body.addEventListener('keydown', (event) => {
+    const key = parseInt(event.detail || event.which)
+    if (code.includes(key) && (code[idx] === key)){
+      console.log(event.which)
+      idx++
+      if (idx === (code.length-1)){
+        alert("Code entered!")
+        idx = 0
+      }
+    } else {
+      idx = 0
+    };
+  })
+};
